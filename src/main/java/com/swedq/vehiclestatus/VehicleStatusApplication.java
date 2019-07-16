041b16ac7b31fa6b2466e097dbc6f876848f414b
José Kleiton da Silva <kleiton.contato@gmail.com>
2019-07-16 20:56:29 -0300

    first commit

---
+++ src/main/java/com/swedq/vehiclestatus/VehicleStatusApplication.java
@@ -0,0 +1,13 @@
+package com.swedq.vehiclestatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swedq/vehiclestatus/VehicleStatusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swedq.vehiclestatus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VehicleStatusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
